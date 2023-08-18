b4a3fffc643db28ff841834e71dd84908181a610
Hassan Fathy <hassanfathy460@gmail.com>
2023-08-18 22:06:55 +0300

    create async thunk and make actions

--- src/store/bookSlice.js
+++ src/store/bookSlice.js
@@ -1,10 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getBooks = createAsyncThunk("books/getBooks", async (_, thunkApi) => {
+    try {
+        const res = await fetch("http://localhost:3009/books");
+        const data = await res.json();
+        return data;
+    } catch {
+        console.log("error getting books")
+    }
+})
 
 const bookSlice = createSlice({
     name: "book",
     initialState: { books: null },
     reducers: {
-
+        [getBooks.pending]: (state, action) => {
+            console.log(action);
+        },
+        [getBooks.fulfilled]: (state, action) => {
+            console.log(action);
+        },
+        [getBooks.rejected]: (state, action) => {
+            console.log(action);
+        }
     }
 })
 
